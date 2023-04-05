35a5f016594adc650da8b6b5e98b080adf789433
BlackArcherr <70406811+BlackArcherr@users.noreply.github.com>
2023-04-05 21:20:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import lognorm
 
 chat_id = 474140315 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    mu, sigma = lognorm.fit(x-139, floc=0)
-    return mu # Ваш ответ
+    y = x-139
+    return  np.mean(y)# Ваш ответ
