9d0526d196d00dd61f30663c853ee3c42b1528de
BlackArcherr <70406811+BlackArcherr@users.noreply.github.com>
2023-04-05 21:45:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 chat_id = 474140315 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    y = x-139
-    return  np.mean(np.exp(y))# Ваш ответ
+    x = x - 139
+    log_x = np.log(x)
+    mu = np.mean(log_x)
+    s = np.std(log_x, ddof=1)
+    alpha = np.exp(mu - s**2/2)
+    return alpha
